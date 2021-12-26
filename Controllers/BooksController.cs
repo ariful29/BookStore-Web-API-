1309fcd57dfa252812f160abe383f86dda3cb15c
ARIF <omarifulislam@gmail.com>
2021-12-26 12:08:05 +0600

    initial commit

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,43 @@
+﻿using BookStoreAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        public List<Book> books = new List<Book>()
+       {
+
+new Book {Id= 1, Title = "Sesher Kobiti", Author= "Rabindranath", PublicationYear = 2021, CallNumber = "Fatra" },
+new Book {Id= 2, Title = "Ager Kobita", Author= "Raju", PublicationYear = 1921, CallNumber = "Valo" },
+new Book {Id= 3, Title = "Padma Nadir Majhi", Author= "Emdad", PublicationYear = 2002, CallNumber = "Normal" },
+new Book {Id= 4, Title = "Hajar Bachar Dhore", Author= "Shaon", PublicationYear = 1990, CallNumber = "Avg" },
+new Book {Id= 5, Title = "Roktatto Prantor", Author= "Tarek", PublicationYear = 1980, CallNumber = "Fatra" },
+new Book {Id= 6, Title = "Hoimonty", Author= "Krishno", PublicationYear = 2011, CallNumber = "Normal" },
+new Book {Id= 7, Title = "Bilashi", Author= "Dipto", PublicationYear = 2001, CallNumber = "Good" }
+
+};ss
+        [HttpGet]
+        public ActionResult<IEnumerable<Book>> GetAllBooks()
+        {
+            return books;
+        }
+
+        [HttpGet ("{id}")]
+        public ActionResult<Book> GetBook(int id)
+        {
+            var book = books.FirstOrDefault(x => x.Id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return book;
+        }
+
+
+
+    }
+}
---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,35 @@
+﻿ using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "Value2", "Value3", "Value4", "Value5" };
+        }
+
+        [HttpGet("{id}")]
+
+        public string Get(int id)
+        {
+            return "The value is" + id;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
